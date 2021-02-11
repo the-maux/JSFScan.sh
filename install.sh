71f4737eb7ecb64ea5dc83e1a65857d47aba3f42
the-maux <maxime.renaud@epitech.eu>
2021-02-11 16:48:09 +0100

    Adding Github actions support

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ cat ./Interlace/requirements.txt > requirements.txt
 cat ./SecretFinder/requirements.txt >> requirements.txt
 cat ./LinkFinder/requirements.txt >> requirements.txt
 pip3 install -r requirements.txt || exit
-python3 ./Interlace/setup.py install || exit
-python3 ./LinkFinder/setup.py install || exit
+cd Interlace && python3 ./Interlace/setup.py install || exit && cd -
+cd LinkFinder && python3 ./LinkFinder/setup.py install || exit && cd -
 
 #INSTALL GAU
 echo -e "\n-----------------------INSTALLING GAU------------------------"
