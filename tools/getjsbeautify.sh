9de020ac603bab93442edf744d89352b1efa4941
the-maux <maxime.renaud@epitech.eu>
2021-02-15 21:38:13 +0100

    Adding Github actions support

--- tools/getjsbeautify.sh
+++ tools/getjsbeautify.sh
@@ -3,4 +3,4 @@
 filename=$(echo $1 | awk -F/ '{print $(NF-0)}')
 echo "jsbeautify ouput: $((filename))"
 python3 ./tools/jsbeautify.py $1 $filename
-mv "$filename" ./jsfiles/
+mv "\'$((filename))\'" ./jsfiles/
