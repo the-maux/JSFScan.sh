601263702ff383324015500b2d459ad5ae844632
the-maux <maxime.renaud@epitech.eu>
2021-02-15 15:16:11 +0100

    Adding Github actions support

--- tools/jsbeautify.py
+++ tools/jsbeautify.py
@@ -20,16 +20,14 @@ def main():
     if len(sys.argv) != 3:
         sys.exit(print("\nUsage:\tpython3 {0} <url> <output>\n".format(sys.argv[0])))
     url = sys.argv[1]
-    output = sys.argv[2]
     if '.js' in url:
         http_response = getjs(url)
         if http_response is not None:
             js = beauty(http_response.content)
-            if output:
-                _file = open(sys.argv[2], "w")
-                _file.write(js)
-                _file.close()
-                print("Done! file saved here -> \"{0}\"".format(_file.name))
+            _file = open(sys.argv[2], "w")
+            _file.write(js)
+            _file.close()
+            print("Done! file saved here -> \"{0}\"".format(_file.name))
     else:
         sys.exit(print("\".js\" not found in URL ({}).. check your url".format(sys.argv[1])))
 
