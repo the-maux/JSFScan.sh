731ef474bc51fbd9b0fe7db592d4cba11fcc7aa3
the-maux <maxime.renaud@epitech.eu>
2021-02-15 19:57:47 +0100

    Adding Github actions support

--- JSFScan.sh
+++ JSFScan.sh
@@ -31,7 +31,7 @@ gather_js() {
 
 #Gather Endpoints From JsFiles
 endpoint_js() {
-  interlace -tL urls.txt -threads 5 -c "echo 'python3 ./tools/LinkFinder/linkfinder.py -d -i _target_ -o cli'" --silent --no-bar
+  interlace -tL urls.txt -threads 5 -c "python3 ./tools/LinkFinder/linkfinder.py -d -i _target_ -o cli >> endpoints.txt" --silent --no-bar
   number_of_endpoint_found=$(cat endpoints.txt | wc -l)
   echo "Number of endpoint found: $((number_of_file_found))"
   if [ $number_of_endpoint_found = "0" ]
