0aa20e90bcbfeb3fe83d7001bd976e33cb20e782
the-maux <maxime.renaud@epitech.eu>
2021-02-16 16:39:04 +0100

    Adding Github actions support

--- tools/jsbeautify.py
+++ tools/jsbeautify.py
@@ -4,6 +4,7 @@ import sys
 import jsbeautifier
 import requests
 import json
+from threading import Thread
 
 URLS_FILE_PATH = "./tools/urls_tmp.txt"
 
@@ -17,22 +18,38 @@ def getjs(url):
     return http_response
 
 
-def new_way():
-    urls = list()
-    with open(URLS_FILE_PATH, 'r') as f:
-        for line in f:
-            urls.append(line.strip())
-
-    rax = 0
+def thread_func(urls):
     for url in urls:
         if url.endswith('.js'):
             response = getjs(url)
             if response is not None:
-                rax = rax + 1
                 js = jsbeautifier.beautify(response.content.decode())
-                with open(f"./tools/jsfiles/file-{rax}", "w") as outfile:
+                nameFile = url.split('/')[-1]
+                with open(f"./tools/jsfiles/file-{nameFile}", "w") as outfile:
                     json.dump(js, outfile)
-                print(f"Done! file saved here -> {outfile}", flush=True)
+                print(f"Done! file saved here -> {outfile.name}", flush=True)
+
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+
+def new_way():
+    urls = list()
+    listOfThread = list()
+    with open(URLS_FILE_PATH, 'r') as f:
+        for line in f:
+            urls.append(line.strip())
+
+    listOfurlsSplitted = list(split(urls, 5))
+
+    for listOfurls in listOfurlsSplitted:
+        t = Thread(target=thread_func, args=(listOfurls))
+        listOfThread.append(t)
+        t.start()
+    for thread in listOfThread:
+        thread.join()
 
 
 if __name__ == "__main__":
