3714bc2519e3b6c3aac50659d08041e54507f828
the-maux <maxime.renaud@epitech.eu>
2021-02-23 18:52:38 +0100

    test(gau) input from assetfinder.txt

--- tools/sendReportByMail.py
+++ tools/sendReportByMail.py
@@ -5,7 +5,6 @@ from email.message import EmailMessage
 
 
 def sendMail():
-    print('(DEBUG) Process: Send report to the user by mail: OK')
     message = EmailMessage()
     sender = os.environ['USER_EMAIL']
     recipient = os.environ['USER_EMAIL']
@@ -17,16 +16,15 @@ def sendMail():
     message.set_content(body)
     mime_type, _ = mimetypes.guess_type('report.html')
     mime_type, mime_subtype = mime_type.split('/')
-    with open('report.html', 'rb') as file:
+    with open('report.html', 'r') as file:
         message.add_attachment(file.read(),
                                maintype=mime_type,
                                subtype=mime_subtype,
-                               filename='report.html')
-    print(message)
-    mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
-    mail_server.login(os.environ['USER_EMAIL'], os.environ['USER_PASSWORD'])
-    mail_server.send_message(message)
-    mail_server.quit()
+                               filename='urls.txt')
+        mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
+        mail_server.login(os.environ['USER_EMAIL'], os.environ['USER_PASSWORD'])
+        mail_server.send_message(message)
+        mail_server.quit()
     print('(DEBUG) Sending report to the user by mail: OK')
 
 
