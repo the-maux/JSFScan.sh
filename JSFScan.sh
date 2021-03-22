4a80c300cda7bd76fd718836408238bf77a51f31
the-maux <9989506+the-maux@users.noreply.github.com>
2021-03-22 19:12:38 +0100

    Update JSFScan.sh

--- JSFScan.sh
+++ JSFScan.sh
@@ -1,190 +1,125 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Todo check if the output directory already exist, caused it failed if yes
-# Todo make a proctection when no .js file are found, to avoir the infinite loop in C.I
-# Todo detect if targets in target.txt have the https://, if not: add it
+deep_and_long_recon() {
+  echo -e "\e[36m[+] Searching JsFiles-links mixing gau & subjs & assetfinder \e[0m"
+  cat target.txt | sed 's$https://$$' | chaos -silent | waybackurls | httpx -silent > chaos.txt
+  # TOOLONG: xargs -I@ -P20 sh -c 'gospider -a -s "@" -d 2' | grep -Eo "(http|https)://[^/"].*.js+" | sed "s#] > chaos.txt #TODO add in all urls.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) chaos + wayback found: $(cat chaos.txt | wc -l) url(s)"
 
-echo -e "\e[36m _______ ______ _______ ______                          _     \e[0m"
-echo -e "\e[36m(_______/ _____(_______/ _____)                        | |    \e[0m"
-echo -e "\e[36m     _ ( (____  _____ ( (____   ____ _____ ____     ___| |__  \e[0m"
-echo -e "\e[36m _  | | \____ \|  ___) \____ \ / ___(____ |  _ \   /___|  _ \ \e[0m"
-echo -e "\e[36m| |_| | _____) | |     _____) ( (___/ ___ | | | |_|___ | | | |\e[0m"
-echo -e "\e[36m \___/ (______/|_|    (______/ \____\_____|_| |_(_(___/|_| |_|\e[0m\n"
+  # Removing hakrawler, cause its take too long for github actions ... more than 6hours :(
+  #  cat target.txt | hakrawler -js -plain -usewayback -depth 3 -scope subs | unew > hakrawlerHttpx.txt
+  #  echo -e "(INFO) hakrawler + wayback found: $(cat hakrawlerHttpx.txt | wc -l) url(s)"
 
-############################################  RECON PART   #############################################################
-
-combine_subDomainizer_assetfinder_gau_subjs() {  # mixing SubDomainizer + assetfinder + gau + subjs together
-
-
-  python3 ./SubDomainizer/SubDomainizer.py -l target.txt -o urls_no_http.txt -san all
-  echo -e "(DEBUG) SubDomainizer found: $(cat urls_no_http.txt | wc -l) subdomain (s)"
-
-  cat urls_no_http.txt | assetfinder | sort -u > assetfinder.txt
-  echo -e "(DEBUG) SubDomainizer + assetfinder found: $(cat assetfinder.txt | wc -l) subdomain (s)"
-
-  cat assetfinder.txt | gau -subs -b png,jpg,jpeg,html,txt,JPG | sort -u > gau.txt
-  echo -e "(DEBUG) SubDomainizer + assetfinder + gau found: $(cat gau.txt | wc -l) url(s)"
-
-  cat gau.txt | subjs | grep -v '?v=' | sort -u > subjs.txt
-  echo -e "(DEBUG) SubDomainizer + assetfinder + gau + subjs found: $(cat subjs.txt | wc -l) javascript file(s)"
-}
-
-#Gather JSFilesUrls with gau / subjs / assetfinder / gospider / hakrawler
-use_recontools_individualy() {
-  # TOKNOW: assetfinder is not working good with "https://"
-  cat target.txt | gau | grep -iE "\.js$" | sort -u > gau_solo_urls.txt
-  echo -e "(DEBUG) gau individually found: $(cat gau_solo_urls.txt | wc -l) url(s)"
-
-  cat gau_solo_urls.txt | subjs > subjs_url.txt
-  echo -e "(DEBUG) gau + subjs found: $(cat subjs_url.txt | wc -l) url(s)"
-
-  cat target.txt | sed 's$https://$$' | assetfinder -subs-only | httpx -timeout 3 -threads 300 --follow-redirects -silent | xargs -I% -P10 sh -c 'hakrawler -plain -linkfinder -depth 5 -url %' | awk '{print $3}' | grep -E "\.js(?:onp?)?$" | sort -u > assetfinder_urls.txt
-  echo -e "(DEBUG) assetfinder individually found: $(cat assetfinder_urls.txt | wc -l) url(s)"
+  #cat target.txt | sed 's$https://$$' | assetfinder -subs-only | httpx -timeout 3 -threads 300 --follow-redirects -silent | sort -u > assetfinder_urls.txt
+  #TOLONG: | xargs -I% -P10 sh -c 'hakrawler -plain -linkfinder -depth 5 -url %' | awk '{print $3}' | grep -E "\.js(?:onp?)?$" |
 
   # TOKNOW: gospider is not working good without the "https://"
-  gospider -a -w -r -S target.txt -d 3 | grep -Eo "(http|https)://[^/\"].*\.js+" | sed "s#\] \- #\n#g" > gospider_url.txt
-  echo -e "(DEBUG) gospider individually found: $(cat gospider_url.txt | wc -l) url(s)"
-
-  cat target.txt | hakrawler -js -depth 2 -scope subs -plain > hakrawler_urls.txt
-  echo -e "(DEBUG) hakrawler individually found: $(cat hakrawler_urls.txt | wc -l) url(s)"
+  #gospider -a -w -r -S target.txt -d 3 | grep -Eo "(http|https)://[^/\"].*\.js+" | sed "s#\] \- #\n#g" > gospider_url.txt
+  #echo -e "(INFO) gospider individually found: $(cat gospider_url.txt | wc -l) url(s)"
+  #cat gospider_url.txt >> all_urls.txt
+  #cat hakrawlerHttpx.txt >> all_urls.txt
 }
 
-recon_js_url() {
-  use_recontools_individualy # result in gau_solo_urls.txt subjs_url.txt hakrawler_urls.txt gospider_url.txt
-  cat gau_solo_urls.txt >> all_urls.txt;
-  cat gau_solo_urls.txt >> all_urls.txt;
-  cat subjs_url.txt >> all_urls.txt;
-  cat hakrawler_urls.txt >> all_urls.txt;
-  cat gospider_url.txt >> all_urls.txt;
-  cat subjs.txt >> all_urls.txt
+use_recontools_individualy() {
+  echo -e "\n\e[36m[+] Searching with chaos & gau & subjs & hakrawler & assetfinder & gospider \e[0m"
+  #TODO: combine all this tool to maximize the result, for the just run 1 by 1 than filtering result for duplicates
+  target=$(head -n 1 target.txt | sed 's$https://$$')
 
-  echo "(INFO) Removing dead links with httpx & filtering duplicate url"
-  cat all_urls.txt | httpx -follow-redirects -status-code -silent | grep "[200]" | cut -d ' ' -f1 | sort -u | grep -v '?v=' > urls_alive.txt
+  python3 ./tools/SubDomainizer/SubDomainizer.py -l target.txt -o SubDomainizer.txt -san all  &> nooutput
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) SubDomainizer found: $(cat SubDomainizer.txt | wc -l) domain(s) in scope"
 
-  cat urls_alive.txt | grep -v "jquery" > urls.txt  # filtering classic lib with no impact
+  subfinder -d $target -silent > subfinder.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) subfinder found: $(cat subfinder.txt | wc -l) domain(s) in scope"
+  cat subfinder.txt >> SubDomainizer.txt
+
+  python3 ./tools/Sublist3r/sublist3r.py -d $target -o sublist3r.txt &> nooutput
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) sublist3r found: $(cat sublist3r.txt | wc -l) domain(s) in scope"
+  cat sublist3r.txt >> SubDomainizer.txt
+
+  #TOKNOW: assetfinder is not working good with "https://"
+  cat target.txt | sed 's$https://$$' | assetfinder -subs-only | sort -u > assetfinder_urls.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) assetfinder individually found: $(cat assetfinder_urls.txt | wc -l) url(s) in scope"
+  cat assetfinder_urls.txt >> SubDomainizer.txt
+
+  cat target.txt | sed 's$https://$$' | chaos -silent > chaos.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) chaos found: $(cat chaos.txt | wc -l) url(s)"
+  cat chaos.txt >> SubDomainizer.txt
+
+  cat SubDomainizer.txt | sed 's$www.$$' | sort -u > urls_no_http.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) After filtering duplicate, $(cat urls_no_http.txt | wc -l) domain(s) in scope"
+
+  cat all_url.txt | sort -u > all_urls.txt
+}
+
+#Gather new endpoints From domain / path / JsFiles found
+search_jsFile_from_domain_found() {
+  echo -e "\e[36m[+] Started gathering Js files from domain and path found \e[0m"
+  # Using subjs
+  cat SubDomainizer.txt | sed 's$https://$$' | sed 's$www.$$' | sort -u > listOfDomains.txt
+
+  cat listOfDomains.txt | gau -subs -b png,jpg,jpeg,html,txt,JPG | subjs  |  awk -F '\?' '{print $1}' | sort -u > subjs_url.txt
+  echo test
+  #TODO: with gau filter to get only domain, not all false positive... with subjs its just a wast of time ...
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) gau + subjs found: $(cat subjs_url.txt | wc -l) url(s)"
+  cat subjs_url.txt >> all_js_files_found.txt
+
+  jsubfinder -f listOfDomains.txt  > jsubfinder.txt #TODO add in all urls.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) jsubfinder individually found: $(cat jsubfinder.txt | wc -l) url(s)"
+  cat jsubfinder.txt >> all_js_files_found.txt
+
+  #TOKNOW: regroup found of subjs &  jsubfinder & LinkFinder
+
+  # Using hakrawler
+  cat listOfDomains.txt | hakrawler -js -depth 2 -scope subs -plain >> hakrawler_urls.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo -e "(INFO) hakrawler individually found: $(cat hakrawler_urls.txt | wc -l) url(s)"
+  cat hakrawler_urls.txt >> all_js_files_found.txt
+
+  # TOKNOW: linkfinder doesnt work if https is not present
+  cat all_js_files_found.txt | sed 's$https://$$' | awk '{print "https://" $0}' > search_endpoint.txt # tobe sur there are alway a https://
+  interlace -tL search_endpoint.txt -threads 5 -c "python3 ./tools/LinkFinder/linkfinder.py -d -i '_target_' -o cli >> all_endpoints.txt" --silent --no-bar
+  number_of_endpoint_found=$(cat all_endpoints.txt | wc -l)
+  if [ $number_of_endpoint_found = "0" ]
+  then
+      echo -n `date +"[%m-%d %H:%M:%S]"` && echo "(WARNING) No endpoint found"
+  fi
+  cat all_endpoints.txt | sort -u > endpoints.txt
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo "(INFO) Number of endpoint found with LinkFinder: $(cat endpoints.txt | wc -l)"
+}
+
+regroup_found_and_filter() {
+  echo -e "\e[36m[+] Filtering results \e[0m"
+
+  number_of_file_found=$(cat all_urls.txt | wc -l)
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo "(INFO) Before filtering duplicate/offline/useless files, we found: $((number_of_file_found)) files to analyse"
+
+  # filtering dead link
+  cat all_js_files_found.txt | httpx -follow-redirects -status-code -silent | grep "[200]" | cut -d ' ' -f1 > urls_alive.txt
+
+  # filtering duplicate & libs with no impact
+  cat all_js_files_found.txt | awk -F '?' '{ print $1 }' | grep -v "jquery" | grep $(cat target.txt | sed 's$https://$$') | sort -u > urls.txt
   number_of_file_found=$(cat urls.txt | wc -l)
-  echo "(INFO) After filtering duplicate/offline/boring js files, we found: $((number_of_file_found)) files to analyse"
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo "(INFO) Result of filters: Found $((number_of_file_found)) javascripts files to analyse"
+
+  cat urls.txt | grep $(cat target.txt | sed 's$https://$$') > urls_filter.txt  # Only take if target domain is present
+  number_of_file_found_post_filter=$(cat urls_filter.txt | wc -l)
+  echo -n `date +"[%m-%d %H:%M:%S]"` && echo "(INFO) Result of BIG filters: Found $((number_of_file_found_post_filter)) javascripts files to analyse"
 
   if [ $number_of_file_found = "0" ]
   then
       echo "(ERROR) No JS file found during recon, Exiting..."
-      exit 1
+      #exit 1
   fi
-  cat urls.txt
 }
-
-#Gather Endpoints From JsFiles
-endpoint_js() {
-  # TOKNOW: linkfinder doesnt work if https is present
-  interlace -tL urls_no_http.txt -threads 5 -c "python3 ./tools/LinkFinder/linkfinder.py -d -i _target_ -o cli >> all_endpoints.txt" --silent --no-bar
-  number_of_endpoint_found=$(cat all_endpoints.txt | wc -l)
-  if [ $number_of_endpoint_found = "0" ]
-  then
-      echo "(WARNING) No endpoint found"
-  fi
-  cat all_endpoints.txt | sort -u > endpoints.txt
-  echo "(INFO) Number of endpoint found: $(cat endpoints.txt | wc -l)"
-}
-
-#Collect Js Files For Maually Search
-DownloadJSFile() {
-  mkdir -p /root/jsfiles
-  python3 ./tools/jsbeautify.py
-  echo "(INFO) DownloadJSFile downloaded: $(ls -l /root/jsfiles/ | wc -l) files"
-}
-
-#Gather JSFilesWordlist
-wordlist_js() {
-  cat urls.txt | python3 ./tools/getjswords.py >> temp_jswordlist.txt
-  cat temp_jswordlist.txt | sort -u >> jswordlist.txt
-  echo "(INFO) getjswords found $(cat jswordlist.txt | wc -l) JSWord(s)"
-  rm temp_jswordlist.txt
-}
-
-############################################  ANALYSE SECTION ##########################################################
-#Gather Variables from JSFiles For Xss
-var_js() {
-  cat urls.txt | while read url; do bash ./tools/jsvar.sh $url | tee -a js_var.txt; done
-  echo "(INFO) Var with possible xss :$(cat js_var.txt | wc -l)"
-}
-
-#Find DomXSS
-domxss_js() {
-  interlace -tL urls.txt -threads 5 -c "bash ./tools/findomxss.sh _target_" --silent --no-bar
-}
-
-#Gather Secrets From Js Files
-secret_js() {
-  interlace -tL urls.txt -threads 5 -c "python3 ./tools/SecretFinder/SecretFinder.py -i _target_ -o cli >> jslinksecret.txt" --silent --no-bar
-  echo -n "(INFO) Number of secrets found: " && cat jslinksecret.txt | wc -l
-}
-
-############################################  REPORT SECTION ###########################################################
-
-#Save in Output Folder
-output() {
-  dir=$OUTPUT_DIR
-  mkdir -p $dir
-  mv -vf endpoints.txt all_urls.txt jslinksecret.txt urls.txt jswordlist.txt js_var.txt domxss_scan.txt report.html $dir/
-  mv -v jsfiles/ $dir/
-  tar -cvf archive.tar $dir/
-}
-
-send_to_issue() {
-  token=$GITHUB_TOKEN
-  repo=the-maux/JSFScan.sh
-
-  upload_url=$(curl -s -H "Authorization: token $token"  \
-     -d '{"tag_name": "test", "name":"release-0.0.1","body":"this is the result of the scan"}'  \
-     "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
-
-  upload_url="${upload_url%\{*}"
-
-  echo "uploading asset to release to url : $upload_url"
-
-  curl -s -H "Authorization: token $token"  \
-        -H "Content-Type: application/zip" \
-        --data-binary @archive.tar  \
-        "$upload_url?name=archive.tar&label=JSHUNT"
-}
-
-export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
 recon() {  # Try to gain the maximum of uniq JS file from the target
   echo "Searching JSFiles on target(s):" && cat target.txt
-  echo -e "\n\e[36m[+] Searching JsFiles-links individualy gau & subjs & hakrawler & assetfind & gospider \e[0m"
-  recon_js_url
-  echo -e "\e[36m[+] Searching JsFiles-links mixing gau & subjs & assetfinder \e[0m"
-  combine_subDomainizer_assetfinder_gau_subjs  # result in subjs.txt
-  echo -e "\e[36m[+] Started gathering Endpoints\e[0m"
-  endpoint_js
-  echo -e "\e[36m[+] Started to Gather JSFiles locally for Manual Testing\e[0m"
-  DownloadJSFile
-}
-
-
-analyse() {
-  echo -e "\e[36m[+] Started Gathering Words From JsFiles-links For Wordlist.\e[0m"
-  wordlist_js
-  echo -e "\e[36m[+] Started Finding Varibles in JSFiles For Possible XSS\e[0m"
-  var_js
-  echo -e "\e[36m[+] Scanning JSFiles For Possible DomXSS\e[0m"
-  domxss_js
-  echo -e "\e[36m[+]  Started Finding Secrets in JSFiles\e[0m"
-  secret_js
-  #Extract path to js
-  #cat file.js | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u
-}
-
-report() {
-  echo -e "\e[36m[+]  Generating Html Report!\e[0m"
-  bash report.sh
-  echo -e "\e[36m[+]  Generating output directory!\e[0m"
-  output
-  echo -e "\e[36m[+]  Sending report to github project  !\e[0m"
-  send_to_issue
+  use_recontools_individualy # result in gau_solo_urls.txt subjs_url.txt hakrawler_urls.txt gospider_url.txt
+  #deep_and_long_resddcon
+  search_jsFile_from_domain_found
+  regroup_found_and_filter
+  cat urls.txt > report.html
+  echo -e "\e[36m[+] Sending result by mail \e[0m"
+  python3 tools/sendReportByMail.py
 }
 
 recon
-#analyse
-#report
